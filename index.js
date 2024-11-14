6d975afa9c56143f170c73267e2156e8f8bcc1e0
张帅强 <shuaiqiang.zhang@purplevineip.com>
2024-11-14 10:58:14 +0800

    refactor: 代理接口

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const logger = morgan("tiny");
 
@@ -12,34 +12,29 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+app.use('/api1', createProxyMiddleware({
+  target: "https://api.f2gpt.com/v1/chat/completions", // 目标API的URL
+  changeOrigin: true, // 修改请求头的origin为目标API的origin
+  pathRewrite: {
+    '^/api1': '', // 去掉代理请求中的 `/api` 前缀
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    // 如果目标API需要API_KEY，可以在请求头中添加
+    // if (process.env.OPENAI_API_KEY) {
+      proxyReq.setHeader('Authorization', `Bearer ${process.env.MY_KEY}`);
+      res.status(500).json({ error: 'Proxy error occurred' });
+    // }
+  },
+  onError: (err, req, res) => {
+    console.error('Proxy error:', err);
+    res.status(500).json({ error: 'Proxy error occurred' });
+  }
+}));
+
+
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
 });
 
 // 小程序调用，获取微信 Open ID
@@ -52,7 +47,7 @@ app.get("/api/wx_openid", async (req, res) => {
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
+  // await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
